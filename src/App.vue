7b9f1a578f0b72184be0bd0eaac98a4e1c91cd2d
Andrea Vargas <andreavargasserpa1987@gmail.com>
2025-10-01 00:18:24 -0300

    creada la primer parte de educacion

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NavBar from './components/icons/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/Educacion.vue';
 </script>
 
 <template>
@@ -12,7 +13,10 @@ import DatosPersonales from './components/DatosPersonales.vue';
 
   </header>
   <main>
-    <section id="#educacion"><h2>Educacion</h2></section>
+    <!--el estilo css de sction se encuentra en base.css-->
+    <section id="#educacion"><h2>Educacion</h2>
+    <Educacion />
+    </section>
     <section id="#experiencia"><h2>Experiencia</h2></section>
     <section id="#proyectos"><h2>Proyectos</h2></section>
     <section id="#habilidades"><h2>Habilidades</h2></section>
